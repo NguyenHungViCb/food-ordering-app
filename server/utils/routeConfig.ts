8311d3db5bdf0423d45cd0ca8a143604b7aa8dc3
VI Nguyen <nguyenhungvicb@gmail.com>
2022-04-04 17:15:06 +0700

    route description decorator

--- server/utils/routeConfig.ts
+++ server/utils/routeConfig.ts
@@ -9,23 +9,27 @@ export type RouteConfig = {
 
 const RouteSymbol = Symbol("RouteConfig");
 
-export const routeConfig = ({
-  method,
-  path,
-  middlewares,
-}: RouteConfig): MethodDecorator => {
+export const routeConfig = (config: RouteConfig): MethodDecorator => {
   return (
     target: any,
     propertyKey: string | symbol,
     descriptor: PropertyDescriptor
   ) => {
     target[RouteSymbol] = target[RouteSymbol] || new Map();
-    target[RouteSymbol].set(propertyKey, {
-      descriptor,
-      method,
-      path,
-      middlewares,
-    });
+    if (target[RouteSymbol].get(propertyKey)) {
+      target[RouteSymbol].set(propertyKey, {
+        descriptor,
+        propertyKey,
+        ...config,
+        ...target[RouteSymbol].get(propertyKey),
+      });
+    } else {
+      target[RouteSymbol].set(propertyKey, {
+        descriptor,
+        propertyKey,
+        ...config,
+      });
+    }
   };
 };
 
@@ -41,7 +45,12 @@ export function controller<T extends { new (...args: any[]): {} }>(Base: T) {
             method,
             path,
             middlewares,
-          }: RouteConfig & { descriptor: PropertyDescriptor }) => {
+            ...rest
+          }: RouteConfig &
+            RouteDescription & {
+              descriptor: PropertyDescriptor;
+              propertyKey: string | Symbol;
+            }) => {
             const handler: Array<RequestHandler> = [];
             const main = async (
               req: Request,
@@ -58,7 +67,11 @@ export function controller<T extends { new (...args: any[]): {} }>(Base: T) {
             if (middlewares && middlewares.length > 0) {
               handler.push(...middlewares);
             }
-            routeEvent.emit("update_route", { path, method });
+            routeEvent.emit("update_route", {
+              path,
+              method,
+              ...rest,
+            });
             app[method]("/api" + path, handler);
           }
         );
@@ -66,3 +79,29 @@ export function controller<T extends { new (...args: any[]): {} }>(Base: T) {
     }
   };
 }
+
+export type RouteDescription = {
+  request_payload?: object;
+  response_payload?: object;
+  usage?: string;
+  isAuth?: string;
+};
+export const routeDescription = (description: RouteDescription) => {
+  return (
+    target: any,
+    propertyKey: string | symbol,
+    __: PropertyDescriptor
+  ) => {
+    target[RouteSymbol] = target[RouteSymbol] || new Map();
+    if (target[RouteSymbol].get(propertyKey)) {
+      target[RouteSymbol].set(propertyKey, {
+        ...target[RouteSymbol].get(propertyKey),
+        ...description,
+      });
+    } else {
+      target[RouteSymbol].set(propertyKey, {
+        ...description,
+      });
+    }
+  };
+};
